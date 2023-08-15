2e7f2dde1db4c88cbb2c2d61fc1aa72d15f0c746
prasadkongandlapk <prasadkongandlapk@gmail.com>
2023-08-15 03:26:20 +0000

    lkafdj

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -1,19 +1,35 @@
 import Slider from 'react-slick'
 import MovieItem from '../MovieItem'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 const MoviesSlider = props => {
   const {moviesList} = props
   return (
-    <Slider>
-      <ul className="videos-bg">
-        {moviesList.map(each => {
-          if (each.category === 'ACTION') {
-            return <MovieItem actionInfo={each} key={each.id} />
-          }
-          return <MovieItem actionInfo={each} key={each.id} />
-        })}
-      </ul>
-    </Slider>
+    <div>
+      <Slider>
+        <ul className="videos-bg">
+          {moviesList.map(each => {
+            if (each.category === 'ACTION') {
+              return <MovieItem actionInfo={each} key={each.id} />
+            }
+            return null
+          })}
+        </ul>
+      </Slider>
+
+      <Slider>
+        <ul className="videos-bg">
+          {moviesList.map(each => {
+            if (each.category === 'COMEDY') {
+              return <MovieItem actionInfo={each} key={each.id} />
+            }
+            return null
+          })}
+        </ul>
+      </Slider>
+    </div>
   )
 }
 export default MoviesSlider
